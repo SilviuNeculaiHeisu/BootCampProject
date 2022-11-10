022586bafbca6784d767c578bf764deb8db1b67f
Harsa Florin <Harsa.Florin@solera.com>
2022-11-10 13:17:39 +0200

    Main

--- src/TeamCard.js
+++ src/TeamCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import './TeamCard.css'
 
 const TeamCard = () => {
-  return <div className="team-card">
+  return <div className="team-card" onClick={TeamCard}>
     <h2>Team Name</h2>
     <div className="team-card__points">0</div>
   </div>;

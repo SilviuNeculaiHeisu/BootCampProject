ca36c13216bc6b1f7bd21d39ffc2f1e029f98b12
Harsa Florin <Harsa.Florin@solera.com>
2022-11-10 12:34:12 +0200

    main page
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.js
    	new file:   src/TeamCard.css
    	new file:   src/TeamCard.js
    	modified:   src/index.js
    	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TeamCard from './TeamCard';
+
 function App() {
-  return <></>;
+  return <div>
+      <h1>Solera Teams. Bootcamp 4</h1>
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+
+  </div>;
 }
 
 export default App;
---
+++ src/TeamCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './TeamCard.css'
+
+const TeamCard = () => {
+  return <div className="team-card">
+    <h2>Team Name</h2>
+    <div className="team-card__points">0</div>
+  </div>;
+};
+
+export default TeamCard;
